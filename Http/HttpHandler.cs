d9cb3691a6fdd93779f28b2a0ec295d72ee9d7a0
Renan Souza <renan.souza@powerpush.com.br>
2022-05-06 14:32:46 -0300

    first commit

---
+++ Http/HttpHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace GameFlow.Plugins.Http
+{
+    public static class HttpHandler
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private static void InitializeClient()
+        {
+            HttpHandler.client.DefaultRequestHeaders.Accept.Clear();
+            HttpHandler.client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public static async Task<Response> Get(string url)
+        {
+            HttpHandler.InitializeClient();
+            Response response = (Response) await HttpHandler.client.GetAsync(url);
+            return response;
+        }
+
+        public static async Task<Response> Post(string url, Request payload)
+        {
+            HttpHandler.InitializeClient();
+            Response response = (Response) await HttpHandler.client.PostAsync(url, payload);
+            return response;
+        }
+
+        public static async Task<Response> Put(string url, Request payload)
+        {
+            HttpHandler.InitializeClient();
+            Response response = (Response)await HttpHandler.client.PutAsync(url, payload);
+            return response;
+        }
+
+        public static async Task<Response> Delete(string url)
+        {
+            HttpHandler.InitializeClient();
+            Response response = (Response) await HttpHandler.client.DeleteAsync(url);
+            return response;
+        }
+    }
+}
---
+++ Http/Request.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Text;
+
+namespace GameFlow.Plugins.Http
+{
+    public class Request : StringContent
+    {
+        public Request(string content) : base(content) { }
+        public Request(string content, Encoding encoding) : base(content, encoding) { }
+        public Request(string content, Encoding encoding, string mediaType) : base(content, encoding, mediaType) { }
+    }
+}
+
